c240fdf6a09d199a42959ab4079dceeef82d9b20
unknown <afandimboizz@gmail.com>
2023-10-21 11:38:08 +0700

    membuat Caraousel dan membuat navbar lebih bagus

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,31 @@ accordionItems.forEach((item) => {
     content.classList.toggle('hidden');
   });
 });
+
+let currentIndex = 0;
+const items = document.querySelectorAll('.carousel-item');
+const totalItems = items.length;
+console.log(totalItems);
+
+function showSlide(index) {
+  if (index < 0) {
+    currentIndex = totalItems - 1;
+  } else if (index >= totalItems) {
+    currentIndex = 0;
+  } else {
+    currentIndex = index;
+  }
+
+  const translateValue = -currentIndex * 100 + '%';
+  document.querySelector('.carousel').style.transform = `translateX(${translateValue})`;
+}
+
+function nextSlide() {
+  showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+  showSlide(currentIndex - 1);
+}
+
+setInterval(nextSlide, 3500);
